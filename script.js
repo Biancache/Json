a86f883cdea85a8ddec06a35bd429bf8ab2211ab
Bianca Che <xc2455@nyu.edu>
2023-11-16 05:53:45 -0500

    Finish

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-fetch('projects.json')
+fetch('data.json')
     .then(response => response.json())
     .then(projects => {
+        console.log(projects);
         const projectsContainer = document.getElementById('projects-container');
         projects.forEach(project => {
+            console.log(project);
             const projectDiv = document.createElement('div');
             projectDiv.className = 'data';
             projectDiv.innerHTML = `
-                <img src="${Miami.JPG}" alt="${project.title}">
+                <img src="${project.image}" alt="${project.title}" onclick="popImg(this)">
                 <h2>${project.title}</h2>
+                <p>${project.describe}</p>
             `;
             projectsContainer.appendChild(projectDiv);
         });
@@ -15,3 +18,20 @@ fetch('projects.json')
     .catch(error => {
         console.error('Error loading project data:', error);
     });
+
+function popImg(dom) {
+    new Fancybox(
+        // Array containing gallery items
+        [
+            // Gallery item
+            {
+                src: dom.src,
+                thumb: dom.src,
+            },
+        ],
+        // Gallery options
+        {
+            hideScrollbar: false,
+        }
+    );
+}
